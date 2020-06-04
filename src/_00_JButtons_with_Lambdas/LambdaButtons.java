bef5b8ed2519ba9ff39b7e932a85bafd5611cf0b
npase <npase@DESKTOP-053SBUQ.san.rr.com>
2020-06-03 18:27:13 -0700

    ip module2 checkpoint

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,26 +1,47 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
+	private JTextField answerField=new JTextField();
 	
 	public LambdaButtons() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new FlowLayout());
+		answerField.setFont(new Font("Arial",Font.PLAIN,40));
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
-		
+		window.add(answerField);
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((ActionEvent e)->{
+			int first=Integer.parseInt(JOptionPane.showInputDialog("Enter the first number."));
+			int second=Integer.parseInt(JOptionPane.showInputDialog("Enter the second number."));
+			answerField.setText(first+second+"");
+			window.pack();
+		});
+		randNumber.addActionListener((ActionEvent e)->{
+			answerField.setText(new Random().nextInt()+"");
+			window.pack();
+		});
+		tellAJoke.addActionListener((ActionEvent e)->{
+			answerField.setText("This is a joke.");
+			window.pack();
+		});
 		window.setVisible(true);
 		window.pack();
 	}
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,13 +13,46 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String newString="";
+			for (int i = s.length()-1; i >= 0; i--) {
+				newString+=s.charAt(i);
+			}
+			System.out.println(newString);
+		},"message");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String newString="";
+			for (int i = 0; i < s.length(); i++) {
+				String newChar=s.charAt(i)+"";
+				if(i%2==0) {
+					newChar=newChar.toLowerCase();
+				}else {
+					newChar=newChar.toUpperCase();
+				}
+				newString+=newChar;
+			}
+			System.out.println(newString);
+		},"message");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String newString="";
+			for (int i = 0; i < s.length(); i++) {
+				newString+=s.charAt(i)+".";
+			}
+			System.out.println(newString);
+		},"message");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String vowels="aeiou";
+			String newString="";
+			for (int i = 0; i < s.length(); i++) {
+				if(!vowels.contains(s.charAt(i)+"")) {
+					newString+=s.charAt(i);
+				}
+			}
+			System.out.println(newString);
+		},"message");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
